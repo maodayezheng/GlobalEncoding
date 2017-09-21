34307c504ea8c0a3438abe7fea7b046acdb12268
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2017-09-21 18:13:49 +0100

    finished the script for data pre-process, (encapsule later)

--- preprossing.py
+++ preprossing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 from Utils import wordFrequency
@@ -19,7 +20,7 @@ with open("books_in_sentences/books_large_p1.txt", "r") as doc:
             break
 
 # Save selected sentences
-sentences_save_path = "selected" + str(total) + "_origin.txt"
+sentences_save_path = "selected_%d_origin.txt" % total
 with open("data/" + sentences_save_path, "w") as doc:
     for line in selected:
         doc.write(line + "\n")
@@ -27,16 +28,15 @@ with open("data/" + sentences_save_path, "w") as doc:
 # Calculate the word frequency
 vocab = wordFrequency(selected)
 words = []
-for k, v in vocab:
-    if k is not "\n":
-        words.append(k)
+for w, v in vocab:
+    if w is not "\n":
+        words.append(w)
 n_tokens = len(words)
 print(n_tokens)
-k = int(0.1*n_tokens)
 top_k = words[0:50]
 
 # Save top k frequent words 
-top_k_saved_path = "top_" + str(k) + "words.txt"
+top_k_saved_path = "top_%d_words.txt" % k
 with open("data/" + top_k_saved_path, "w") as doc:
     for w in top_k:
         doc.write(w+"\n")
@@ -55,7 +55,7 @@ for s in selected:
     doc_dropped.append(sen)
 
 # Save the dropped sentences
-drop_doc_path = "selected_" + str(total) + "_dropped.txt"
+drop_doc_path = "selected_%d_dropped.txt" % total
 with open("data/"+drop_doc_path, "w") as doc:
     for line in doc_dropped:
         doc.write(line + "\n")
@@ -69,13 +69,17 @@ vocab_dict = tf_vectorizer.vocabulary_
 components = lda.components_
 component_names = tf_vectorizer.get_feature_names()
 
+lda_params = lda.get_params(deep=True)
+with open('data/params/lda_params.save', 'wb') as f:
+    pickle.dump(lda_params, f, protocol=pickle.HIGHEST_PROTOCOL)
+
 # Save topics and their associate words 
 for topic_idx, topic in enumerate(components):
     message = "topic_%d" % topic_idx
     print(message)
     idx = topic.argsort()
     words = []
-    with open("data/selected_500_" + message + ".txt", "w") as doc:
+    with open("data/" + message + ".txt", "w") as doc:
         for i in idx:
             doc.write(component_names[i] + "\n")
 
@@ -85,7 +89,7 @@ tf_sentence_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
                                          vocabulary=vocab_dict)
 
 # Replace words with their topic id and save the results
-topic_sentence_path = "selected_" + str(total) + "_topic_sentence.txt"
+topic_sentence_path = "selected_%d_topic_sentence.txt" % total
 with open("data/" + topic_sentence_path, "w") as doc:
     for line in selected:
         raw_sentence = line.split(" ")
@@ -96,7 +100,7 @@ with open("data/" + topic_sentence_path, "w") as doc:
                 tf = tf_sentence_vectorizer.fit_transform([token])
                 topic_distribution = lda.transform(tf)
                 t_idx = np.argmax(topic_distribution)
-                token = "<topic_" + str(t_idx) + ">"
+                token = "<topic_%d>" % t_idx
             sentence.append(token)
         s = " ".join(sentence)
         print(s)
