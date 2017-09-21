5e012b64670af3577105dfdde6a4dc7661b2acbb
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2017-09-21 17:07:47 +0100

    add the code to replace words with their argmax topic

--- preprossing.py
+++ preprossing.py
@@ -1,8 +1,9 @@
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
-from Utils import wordFrequency, ldaVocab
+from Utils import wordFrequency
 count = 0
-total = 50000
+total = 500
 k = 50
 selected = []
 
@@ -20,7 +21,7 @@ with open("books_in_sentences/books_large_p1.txt", "r") as doc:
 sentences_save_path = "selected" + str(total) + "_origin.txt"
 with open("data/"+ sentences_save_path, "w") as doc:
     for line in selected:
-        doc.write(line + "\n")
+        doc.write(line)
 
 vocab = wordFrequency("data/" + sentences_save_path)
 words = []
@@ -39,7 +40,7 @@ with open("data/" + top_k_saved_path, "w") as doc:
         doc.write(w+"\n")
 
 # Remove top k frequent words
-with open("data/selected_500_raw.txt", "r") as raw, open("data/selected_500.txt", "w") as doc:
+with open("data"+sentences_save_path, "r") as raw, open("data/selected_500.txt", "w") as doc:
     for line in raw:
         raw_sentence = line.split(" ")
         sentence = []
@@ -58,7 +59,7 @@ with open("data/selected_500.txt", "r") as doc:
     for line in doc:
         content.append(line)
     tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
-                                    max_features=None
+                                    max_features=None,
                                     stop_words='english')
     tf = tf_vectorizer.fit_transform(content)
     lda = LatentDirichletAllocation(n_topics=100, max_iter=5,
@@ -67,7 +68,8 @@ with open("data/selected_500.txt", "r") as doc:
                                     random_state=0)
     lda.fit(tf)
 vocab_dict = tf_vectorizer.vocabulary_
-
+components = lda.components_
+component_names = tf_vectorizer.get_feature_names()
 
 # Save topics and their associate words 
 for topic_idx, topic in enumerate(components):
@@ -79,7 +81,12 @@ for topic_idx, topic in enumerate(components):
         for i in idx:
             doc.write(component_names[i] + "\n")
 
-# Replace words with their topic id and save the results 
+tf_sentence_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
+                                         max_features=None,
+                                         stop_words='english',
+                                         vocabulary=vocab_dict)
+
+# Replace words with their topic id and save the results
 with open("data/selected_500_raw.txt", "r") as raw, open("data/selected_500.txt", "w") as doc:
     for line in raw:
         raw_sentence = line.split(" ")
@@ -87,11 +94,11 @@ with open("data/selected_500_raw.txt", "r") as raw, open("data/selected_500.txt"
         for w in raw_sentence:
             token = w
             if token not in top_k:
-                sentence.append(w)
-                # Infer topic with LDA based on a given word 
-                
+                tf = tf_sentence_vectorizer.fit_transform([token])
+                topic_distribution = lda.transform(tf)
+                t_idx = np.argmax(topic_distribution)
+                token = "<topic_" + str(t_idx) + ">"
             sentence.append(token)
         s = " ".join(sentence)
+        print(s)
         doc.write(s + "\n")
-
-print()
